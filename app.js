65c6e83e262ecdb294942fea7b2c31a0e16b5ae7
jripke74 <jripke74@gmail.com>
2025-10-21 11:21:35 -0700

    add insertafter

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class LinkedList {
     }
     this.tail = newNode;
     if (!this.head) {
-      this.head = newNode
+      this.head = newNode;
     }
   }
 
@@ -21,6 +21,56 @@ class LinkedList {
     this.head = newNode;
     if (!this.tail) {
       this.tail = newNode;
+    }
+  }
+
+  insertAfter(value, afterValue) {
+    const existingNode = this.find(afterValue);
+
+    if (existingNode) {
+      const newNode = { value: value, next: existingNode.next }
+      existingNode.next = newNode;
+    }
+  }
+
+  find(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    let curNode = this.head;
+
+    while (curNode) {
+      if (curNode.value === value) {
+        return curNode;
+      }
+      curNode = curNode.next;
+    }
+
+    return null;
+  }
+
+  delete(value) {
+    if (!this.head) {
+      return null;
+    }
+
+    while (this.head && this.head.value === value) {
+      this.head = this.head.next;
+    }
+
+    let curNode = this.head;
+
+    while (curNode.next) {
+      if (curNode.next.value === value) {
+        curNode.next = curNode.next.next;
+      } else {
+        curNode = curNode.next;
+      }
+    }
+
+    if (this.tail.value === value) {
+      this.tail = curNode;
     }
   }
 
@@ -41,8 +91,23 @@ const linkedList1 = new LinkedList();
 linkedList1.append(1);
 linkedList1.append("hello there");
 linkedList1.append("Max");
+linkedList1.append("Max");
 linkedList1.append(true);
 linkedList1.append(18.51);
-linkedList1.prepend("First value");
+linkedList1.prepend("First value!");
+linkedList1.prepend("First value!");
+
+console.log(linkedList1.toArray());
+
+linkedList1.delete("Max");
+linkedList1.delete("First value!");
+linkedList1.delete(18.51);
+
+console.log(linkedList1.toArray());
+console.log(linkedList1.find("Max"));
+console.log(linkedList1.find("hello there"));
+
+linkedList1.insertAfter('new value - 1', 1);
+linkedList1.insertAfter('new value - 2', 'hello there');
 
 console.log(linkedList1.toArray());
