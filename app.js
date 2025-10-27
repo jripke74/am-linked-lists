9d87212699f73561aefc3ed5cabb1c8bfab9dbd7
jripke74 <jripke74@gmail.com>
2025-10-27 13:12:27 -0700

    add deleteHad()

--- app.js
+++ app.js
@@ -1,113 +1,23 @@
-class LinkedList {
+import { LinkedList } from "./linked-list";
+
+class Stack {
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.list = new LinkedList();
   }
 
-  append(value) {
-    const newNode = { value: value, next: null };
-    if (this.tail) {
-      this.tail.next = newNode;
-    }
-    this.tail = newNode;
-    if (!this.head) {
-      this.head = newNode;
-    }
+  push(value) {
+    this.list.push(value);
   }
 
-  prepend(value) {
-    const newNode = { value: value, next: this.head };
-
-    this.head = newNode;
-    if (!this.tail) {
-      this.tail = newNode;
-    }
+  pop() {
+    return this.list.pop();
   }
 
-  insertAfter(value, afterValue) {
-    const existingNode = this.find(afterValue);
-
-    if (existingNode) {
-      const newNode = { value: value, next: existingNode.next }
-      existingNode.next = newNode;
-    }
-  }
-
-  find(value) {
-    if (!this.head) {
-      return null;
-    }
-
-    let curNode = this.head;
-
-    while (curNode) {
-      if (curNode.value === value) {
-        return curNode;
-      }
-      curNode = curNode.next;
-    }
-
-    return null;
-  }
-
-  delete(value) {
-    if (!this.head) {
-      return null;
-    }
-
-    while (this.head && this.head.value === value) {
-      this.head = this.head.next;
-    }
-
-    let curNode = this.head;
-
-    while (curNode.next) {
-      if (curNode.next.value === value) {
-        curNode.next = curNode.next.next;
-      } else {
-        curNode = curNode.next;
-      }
-    }
-
-    if (this.tail.value === value) {
-      this.tail = curNode;
-    }
+  isEmpty() {
+    return this.list.length === 0;
   }
 
   toArray() {
-    const elements = [];
-
-    let curNode = this.head;
-    while (curNode) {
-      elements.push(curNode);
-      curNode = curNode.next;
-    }
-
-    return elements;
+    return this.list.slice();
   }
 }
-
-const linkedList1 = new LinkedList();
-linkedList1.append(1);
-linkedList1.append("hello there");
-linkedList1.append("Max");
-linkedList1.append("Max");
-linkedList1.append(true);
-linkedList1.append(18.51);
-linkedList1.prepend("First value!");
-linkedList1.prepend("First value!");
-
-console.log(linkedList1.toArray());
-
-linkedList1.delete("Max");
-linkedList1.delete("First value!");
-linkedList1.delete(18.51);
-
-console.log(linkedList1.toArray());
-console.log(linkedList1.find("Max"));
-console.log(linkedList1.find("hello there"));
-
-linkedList1.insertAfter('new value - 1', 1);
-linkedList1.insertAfter('new value - 2', 'hello there');
-
-console.log(linkedList1.toArray());
